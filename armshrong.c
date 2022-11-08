1c3b7a73d7fd84642d396c87aa6f371a13bf139d
msuhani <msuhanism@gmail.com>
2022-11-08 15:01:22 +0530

    Create armshrong.c

---
+++ armshrong.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int num=45, sum=0;
+    printf("Enter a no between 1 and 500 : ");
+    scanf("%d",&num);
+    int origin_num=num;
+    while(num>0){
+        int rem=num%10;
+        sum=sum+rem*rem*rem;
+        num=num/10;
+    }
+    if(origin_num==sum)
+        printf("%d IS AN ARMSTRONG NO",origin_num);
+    else
+        printf("%d IS NOT AN ARMSTRONG NO",origin_num);
+}
