6bc7174853cf3c2544637d23a6dd315bfeb3d9f1
msuhani <msuhanism@gmail.com>
2022-11-27 17:00:54 +0530

    arrays

---
+++ arr_bubble_sort_better.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int num;
+    printf("enter how many numbers :");
+    scanf("%d", &num);
+    int arr[num];
+    for(int i=0;i<num;i++){
+        printf("enter the number : ");
+        scanf("%d", &arr[i]);
+    }
+    int temp;
+    printf("the sorted array is \n");
+    for(int i=num;i>0;i--){
+        for(int k=0;k<i-1;k++){
+            if(arr[k]>arr[k+1]){
+                temp=arr[k];
+                arr[k]=arr[k+1];
+                arr[k+1]=temp;
+            }
+        }
+    }
+    for(int j=0;j<num;j++)
+        printf(" %d", arr[j]);
+    
+}
