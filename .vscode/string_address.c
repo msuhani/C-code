a8e6cfe63800cf0465d623856568143ef16d3417
mpankaj-code <mpankaj@gmail.com>
2022-12-28 15:27:28 +0530

    changes

---
+++ .vscode/string_address.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char s[ ] = "Get organised! learn C!!" ;
+    int start = 5, chars = 5;
+    // printf ( "\n%s", &s[2] ) ;
+    // printf ( "\n%s", s ) ;
+    // printf ( "\n%s", &s ) ;
+    // printf ( "\n%c", s[2] ) ;
+    char t[] = &s[start-1];
+    t[chars] = '\0';
+    printf ( "\n%s\n", t ) ;
+    return 0;
+}
